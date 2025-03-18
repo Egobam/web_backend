9497a00576e7a6171428ed9074203a41d9f656b0
egobam <zakas21lol@gmail.com>
2025-03-18 16:53:13 +0300

    change in html+css

--- FourthTask/index.php
+++ FourthTask/index.php
@@ -15,6 +15,7 @@ if (!empty($errors)) {
     <meta charset="UTF-8">
     <title>Форма заявки</title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@400;700&display=swap" rel="stylesheet">
 </head>
 <body>
     <h1>Заполните форму</h1>
@@ -33,7 +34,7 @@ if (!empty($errors)) {
 
     <form action="save.php" method="POST">
         <div class="form-group">
-            <label for="fio">ФИО:</label><br>
+            <label for="fio">ФИО:</label>
             <div class="textInputWrapper">
                 <input type="text" id="fio" name="fio" placeholder="Введите ФИО" value="<?= htmlspecialchars($values['fio'] ?? '') ?>"
                        class="textInput <?= isset($errors['fio']) ? 'error-field' : '' ?>" required>
@@ -44,7 +45,7 @@ if (!empty($errors)) {
         </div>
 
         <div class="form-group">
-            <label for="phone">Телефон:</label><br>
+            <label for="phone">Телефон:</label>
             <div class="textInputWrapper">
                 <input type="tel" id="phone" name="phone" placeholder="Введите телефон" value="<?= htmlspecialchars($values['phone'] ?? '') ?>"
                        class="textInput <?= isset($errors['phone']) ? 'error-field' : '' ?>" required>
@@ -55,7 +56,7 @@ if (!empty($errors)) {
         </div>
 
         <div class="form-group">
-            <label for="email">E-mail:</label><br>
+            <label for="email">E-mail:</label>
             <div class="textInputWrapper">
                 <input type="email" id="email" name="email" placeholder="Введите email" value="<?= htmlspecialchars($values['email'] ?? '') ?>"
                        class="textInput <?= isset($errors['email']) ? 'error-field' : '' ?>" required>
@@ -66,7 +67,7 @@ if (!empty($errors)) {
         </div>
 
         <div class="form-group">
-            <label for="birthdate">Дата рождения:</label><br>
+            <label for="birthdate">Дата рождения:</label>
             <div class="textInputWrapper">
                 <input type="date" id="birthdate" name="birthdate" value="<?= htmlspecialchars($values['birthdate'] ?? '') ?>"
                        class="textInput <?= isset($errors['birthdate']) ? 'error-field' : '' ?>" required>
@@ -77,18 +78,20 @@ if (!empty($errors)) {
         </div>
 
         <div class="form-group radio-group">
-            <label>Пол:</label><br>
-            <input type="radio" id="male" name="gender" value="male" <?= ($values['gender'] ?? '') === 'male' ? 'checked' : '' ?> required>
-            <label for="male">Мужской</label>
-            <input type="radio" id="female" name="gender" value="female" <?= ($values['gender'] ?? '') === 'female' ? 'checked' : '' ?>>
-            <label for="female">Женский</label>
+            <label>Пол:</label>
+            <div class="radio-wrapper">
+                <input type="radio" id="male" name="gender" value="male" <?= ($values['gender'] ?? '') === 'male' ? 'checked' : '' ?> required>
+                <label for="male">Мужской</label>
+                <input type="radio" id="female" name="gender" value="female" <?= ($values['gender'] ?? '') === 'female' ? 'checked' : '' ?>>
+                <label for="female">Женский</label>
+            </div>
             <?php if (isset($errors['gender'])): ?>
                 <span class="error"><?=$errors['gender']?></span>
             <?php endif; ?>
         </div>
 
         <div class="form-group">
-            <label for="languages">Любимый язык программирования:</label><br>
+            <label for="languages">Любимый язык программирования:</label>
             <select id="languages" name="languages[]" multiple class="<?= isset($errors['languages']) ? 'error-field' : '' ?>" required>
                 <?php
                 $langs = ['Pascal', 'C', 'C++', 'JavaScript', 'PHP', 'Python', 'Java', 'Haskell', 'Clojure', 'Prolog', 'Scala', 'Go'];
@@ -104,7 +107,7 @@ if (!empty($errors)) {
         </div>
 
         <div class="form-group">
-            <label for="bio">Биография:</label><br>
+            <label for="bio">Биография:</label>
             <div class="textInputWrapper">
                 <textarea id="bio" name="bio" rows="5" placeholder="Введите биографию" class="textInput <?= isset($errors['bio']) ? 'error-field' : '' ?>" required><?= htmlspecialchars($values['bio'] ?? '') ?></textarea>
             </div>
@@ -116,7 +119,7 @@ if (!empty($errors)) {
         <div class="form-group checkbox-group">
             <label class="checkbox" for="contract">
                 <span class="label">С контрактом ознакомлен(а)</span>
-                <input type="checkbox" id="contract" name="contract" value="yes" <?= isset($values['contract']) ? 'checked' : '' ?> required>
+                <input type="checkbox" id="contract" name="contract" value="yes" <?= ($values['contract'] ?? '') === 'yes' ? 'checked' : '' ?> required>
                 <span class="checkmark"></span>
             </label>
             <?php if (isset($errors['contract'])): ?>
